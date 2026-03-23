1cba1fd0cb50543130fd88fbd072af51bfea4124
axellopezuy-cmd <tu-email-de-github@correo.com>
2026-03-22 21:15:50 -0300

    include WASM build artifacts for panel-browser

---
+++ panel-browser/src/wasm/parser_wasm.d.ts
@@ -0,0 +1,56 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Parse HTML + CSS and return a JSON RenderMessage.
+ * Called from JavaScript with raw file contents.
+ */
+export function parse(html: string, css: string): string;
+
+/**
+ * Parse only CSS, return JSON array of Directriz.
+ */
+export function parse_css_only(css: string): string;
+
+/**
+ * Parse only HTML, return JSON array of ObjetoHtml.
+ */
+export function parse_html_only(html: string): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly parse: (a: number, b: number, c: number, d: number) => [number, number];
+    readonly parse_css_only: (a: number, b: number) => [number, number];
+    readonly parse_html_only: (a: number, b: number) => [number, number];
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
